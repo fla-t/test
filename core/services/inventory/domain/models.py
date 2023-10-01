7dc4e470dff7b738dea8e0cb784b32b8cd302a64
salmanmuzammil <salman.muzammil@tajir-app.com>
2023-10-02 00:24:48 +0500

    see u later

--- core/services/inventory/domain/models.py
+++ core/services/inventory/domain/models.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import List, Optional
 from uuid import uuid4
 
 
@@ -15,20 +16,31 @@ class SKU:
 
 @dataclass
 class InventoryReceiving:
+    """Basically a log of inventory received, the actual quantities will be in the inventory table"""
+
     id: str
-    sku_id: str
-    qty: int
+    cost: int
+    received_from: str
 
     @classmethod
-    def create(cls, sku_id: str, qty: int) -> "InventoryReceiving":
-        return cls(id=str(uuid4()), sku_id=sku_id, qty=qty)
+    def create(cls, cost: int, received_from: str) -> "InventoryReceiving":
+        return cls(id=str(uuid4()), cost=cost, received_from=received_from)
 
 
 @dataclass
-class Inventory:
+class InventoryLog:
+    """
+    This is our main inventory aggregate, basically we are storing each log of entry. not a single
+    state. Why? because states are hard to manage and we need to have a perfect concurrency model to
+    keep it afloat.
+
+    If you are thinking, that computing quantities each time might be a problem, then you are right,
+    but we will probably cache that. For the sake of simiplicity, and I am not going to do that here.
+
+    """
+
+    id: str
     sku_id: str
     qty: int
-
-    @classmethod
-    def create(cls, sku_id: str, qty: int) -> "Inventory":
-        return cls(sku_id=sku_id, qty=qty)
+    receiving_id: Optional[str]
+    sale_id: Optional[str]
