27e08a3226d6a100fd35c985f5eedfac059f94da
salmanmuzammil <salman.muzammil@tajir-app.com>
2023-10-03 19:06:26 +0500

    category commands done

--- core/services/catalog/entrypoint/commands.py
+++ core/services/catalog/entrypoint/commands.py
@@ -1,18 +1,36 @@
-from services.catalog.domain.models import SKU
+from services.catalog.domain.models import SKU, Category
 from services.catalog.entrypoint.unit_of_work import AbstractUnitOfWork
 
 
-def create_sku(uow: AbstractUnitOfWork, name: str, description: str) -> str:
+def create_sku(uow: AbstractUnitOfWork, category_id: str, name: str, description: str) -> str:
     with uow:
-        sku = SKU.create(name, description)
+        sku = SKU.create(category_id, name, description)
         uow.skus.save([sku])
 
     return sku.id
 
 
-def update_sku(uow: AbstractUnitOfWork, sku_id: str, name: str, description: str) -> str:
+def update_sku(
+    uow: AbstractUnitOfWork, sku_id: str, category_id: str, name: str, description: str
+) -> str:
     with uow:
-        sku = SKU(sku_id, name, description)
+        sku = SKU(sku_id, category_id, name, description)
         uow.skus.save([sku])
 
     return sku.id
+
+
+def create_category(uow: AbstractUnitOfWork, name: str) -> str:
+    with uow:
+        category = Category.create(name)
+        uow.categories.save([category])
+
+    return category.id
+
+
+def update_category(uow: AbstractUnitOfWork, category_id: str, name: str) -> str:
+    with uow:
+        category = Category(id=category_id, name=name)
+        uow.categories.save([category])
+
+    return category.id
--- core/services/catalog/entrypoint/commands_test.py
+++ core/services/catalog/entrypoint/commands_test.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 import pytest
 from services.catalog.domain import models as mdl
 from services.catalog.entrypoint import commands as cmd
@@ -5,23 +7,49 @@ from services.catalog.entrypoint.unit_of_work import AbstractUnitOfWork
 
 
 @pytest.mark.parametrize("selected_uow", ["fake_uow", "db_uow"], indirect=["selected_uow"])
-def test_create_sku(selected_uow: AbstractUnitOfWork):
-    sku_id = cmd.create_sku(selected_uow, "name", "description")
+def test_create_sku(selected_uow: AbstractUnitOfWork, drop_skus_fk):
+    category_id = str(uuid4())
+    sku_id = cmd.create_sku(selected_uow, category_id, "name", "description")
 
     with selected_uow as uow:
-        assert uow.skus.get([sku_id]) == [mdl.SKU(sku_id, "name", "description")]
+        assert uow.skus.get([sku_id]) == [mdl.SKU(sku_id, category_id, "name", "description")]
 
 
 @pytest.mark.parametrize("selected_uow", ["fake_uow", "db_uow"], indirect=["selected_uow"])
-def test_update_sku(selected_uow: AbstractUnitOfWork):
-    sku_id = cmd.create_sku(selected_uow, "name", "description")
+def test_update_sku(selected_uow: AbstractUnitOfWork, drop_skus_fk):
+    sku_id = cmd.create_sku(
+        selected_uow, name="name", category_id=str(uuid4()), description="description"
+    )
 
     with selected_uow as uow:
         sku = uow.skus.get([sku_id])
         assert sku == [mdl.SKU(sku_id, "name", "description")]
 
-    sku[0].name = "new_name"
-    sku_id = cmd.update_sku(selected_uow, sku_id, "new_name", "description")
+    new_cat_id = str(uuid4())
+    sku_id = cmd.update_sku(selected_uow, sku_id, new_cat_id, "new_name", "new_description")
 
     with selected_uow as uow:
-        assert uow.skus.get([sku_id]) == sku
+        assert uow.skus.get([sku_id]) == [
+            mdl.SKU(sku_id, new_cat_id, "new_name", "new description")
+        ]
+
+
+@pytest.mark.parametrize("selected_uow", ["fake_uow", "db_uow"], indirect=["selected_uow"])
+def test_create_category(selected_uow: AbstractUnitOfWork):
+    category_id = cmd.create_category(selected_uow, "fresh")
+
+    with selected_uow as uow:
+        assert uow.categories.get([category_id]) == [mdl.Category(category_id, "fresh")]
+
+
+@pytest.mark.parametrize("selected_uow", ["fake_uow", "db_uow"], indirect=["selected_uow"])
+def test_update_sku(selected_uow: AbstractUnitOfWork):
+    category_id = cmd.create_category(selected_uow, "fresh")
+
+    with selected_uow as uow:
+        assert uow.categories.get([category_id]) == [mdl.Category(category_id, "fresh")]
+
+    cmd.update_category(selected_uow, category_id, "frozen")
+
+    with selected_uow as uow:
+        assert uow.categories.get([category_id]) == [mdl.Category(category_id, "frozen")]
