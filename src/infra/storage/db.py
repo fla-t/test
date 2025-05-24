2f9762d420237bd0fea56acf7a83cdaf5a0074f5
fla-t <salman.mzmil@gmail.com>
2025-05-25 04:41:49 +0500

    I think alembic setup is done, or I hope so

--- src/infra/storage/db.py
+++ src/infra/storage/db.py
@@ -1,26 +1,21 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import declarative_base
 
 from src.infra.config import config
 
+Base = declarative_base()
 
-def _db_url() -> str:
-    url = f"{config.db.engine}+asyncpg://"
 
-    if config.db.username:
-        url += f"{config.db.username}"
-        if config.db.password:
-            url += f":{config.db.password}"
-        url += "@"
-
-    url += f"{config.db.host}"
-
-    if config.db.port:
-        url += f":{config.db.port}"
-
-    url += f"/{config.db.database_name}"
+def db_url() -> str:
+    url = (
+        f"{config.db.engine}+asyncpg://"
+        f"{config.db.username}:{config.db.password}"
+        f"@{config.db.host}:{config.db.port}"
+        f"/{config.db.database_name}"
+    )
     return url
 
 
 # One engine and one sessionmaker for the whole application
-engine = create_async_engine(_db_url(), pool_size=10, max_overflow=20, pool_timeout=120)
+engine = create_async_engine(db_url(), pool_size=10, max_overflow=20, pool_timeout=120)
 session_factory = async_sessionmaker(bind=engine, expire_on_commit=True, autoflush=False)
