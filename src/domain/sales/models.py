bb9c84060585ab6257a3ddf6d519ef39480d338a
fla-t <salman.mzmil@gmail.com>
2025-05-25 03:40:31 +0500

    some cosmetics

--- src/domain/sales/models.py
+++ src/domain/sales/models.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 from datetime import datetime
 
+UUID = str
+
 
 @dataclass
 class Sale:
-    id: str
-    product_id: str
+    id: UUID
+    product_id: UUID
     quantity: int
     total_price: float
     created_at: datetime
